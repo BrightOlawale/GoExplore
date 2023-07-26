57854ee150ab801b258925fbcabfd5882ffe29be
Bright Olawale <oolatunjibright@yahoo.com>
2023-07-26 16:27:02 +0100

    [feat: Create slice for request]

--- GoRestAPI/main.go
+++ GoRestAPI/main.go
@@ -1,1 +1,23 @@
-package GoRestAPI
+package main
+
+// Books Creating Book type
+type Books struct {
+	Id    int
+	Title string
+}
+
+// Creating Book data
+var allBooks []Books = []Books{
+	{
+		Id:    1,
+		Title: "The Go Programming Language",
+	},
+	{
+		Id:    2,
+		Title: "Python: A definitive guide",
+	},
+	{
+		Id:    3,
+		Title: "Java: Understanding OOP in Java",
+	},
+}
