0d413cbc7cf84cd3c2b557904cfa93072e7bc1c2
Bright Olawale <oolatunjibright@yahoo.com>
2023-07-27 05:14:16 +0100

    refactor: optimize code structure

--- ExploreGo/customType.go
+++ ExploreGo/customType.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // User :Creating a custom type
 type User struct {
 	Id   int
@@ -8,3 +10,24 @@ type User struct {
 
 // Note that the custom type is exported because it starts with a capital letter
 // If it starts with a lowercase letter, it is not exported
+
+// Function to test the above codes
+func customTypeTest() {
+	// Using the custom type in main function
+	// Assigning values to the custom type
+	firstUser := User{
+		Id:   1,
+		Name: "Olawale",
+	}
+
+	// Printing the values
+	fmt.Println("Id: ", firstUser.Id)
+	fmt.Println("Username: ", firstUser.Name)
+
+	// Using the calculateArea function
+	// Assign values
+	rectangle := Rectangle{width: 5, height: 5}
+
+	// Print the calculated area
+	fmt.Println("Area of the rectangle: ", rectangle.calculateArea())
+}
