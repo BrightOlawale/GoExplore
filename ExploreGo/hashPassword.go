09666c8108cc0a00a6a13093a25118d6c6e7d2b8
Bright Olawale <oolatunjibright@yahoo.com>
2023-07-27 05:14:49 +0100

    feat: password hashing

--- ExploreGo/hashPassword.go
+++ ExploreGo/hashPassword.go
@@ -1,1 +1,53 @@
 package main
+
+// Import bcrypt package
+import "golang.org/x/crypto/bcrypt"
+
+// NewUser Create type for users
+type NewUser struct {
+	Id       int64
+	Username string
+	Password string
+}
+
+// HashPassword Method to hash password of user
+func (user *NewUser) HashPassword() error {
+	// Hash password using bcrypt algorithm
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	// Note that the second argument is the cost of hashing the password, which is set to 10 by default as used above
+	// cost determines the time taken to hash the password, that is the computational power required to hash the password
+	// or say the complexity of the algorithm
+
+	// Check if error occurred
+	if err != nil {
+		return err
+	}
+
+	// Set hashed password to user password
+	user.Password = string(hashedPassword)
+
+	// Return nil if no error occurred
+	return nil
+}
+
+// Function to test the above codes for creating a new user and hashing the password
+func hashTest() {
+	// Create new user
+	newUser := NewUser{
+		Id:       1,
+		Username: "test",
+		Password: "WhatEverYouLike",
+	}
+
+	// Hash password
+	err := newUser.HashPassword()
+
+	// Check if error occurred
+	if err != nil {
+		// Print error
+		println("Error occurred: ", err)
+	}
+
+	// Print password
+	println("Hashed Password: ", newUser.Password)
+}
