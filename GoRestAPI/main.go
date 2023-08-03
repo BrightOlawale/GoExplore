82ceb7c9e01e1ae20da8df12bb1087bfb8babf9a
Bright Olawale <oolatunjibright@yahoo.com>
2023-08-03 23:52:07 +0100

    feat: create new book

--- GoRestAPI/main.go
+++ GoRestAPI/main.go
@@ -83,3 +83,56 @@ func getAllBooks(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+
+// createBook: create a new book in our record
+func createBook(w http.ResponseWriter, r *http.Request) {
+	// Let's check the request method, if it is not POST, we return an error
+	// We can check the request method using the "r.Method" field
+	// which returns the request method
+	// http.MethodPost is a constant that represents the POST method
+	if r.Method != http.MethodPost {
+		// Then response header is set to 405 (Method Not Allowed)
+		// and we use w.Write to write the error message to the response body
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		_, err := w.Write([]byte("Method not allowed"))
+		if err != nil {
+			log.Panic(err)
+		}
+		return
+	}
+
+	// If method is not a problem, then we can proceed to create the book
+	// First, we create a new book variable of type Books
+	var newBook Books
+
+	// Now, we decode the request body into the newBook variable
+	// Note that we use the json.NewDecoder.Decode method to decode the request body
+	// into the newBook variable
+	// The first argument is the request body and the second argument is the variable
+	// we want to decode the request body into
+	err := json.NewDecoder(r.Body).Decode(&newBook)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Now, we append the newBook to the allBooks slice
+	allBooks = append(allBooks, newBook)
+
+	// Now, we encode the newBook variable into JSON format
+	// using the json.Marshal method
+	response, err := json.Marshal(newBook)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Now, we set the response header to 201 (Created)
+	// and use w.Header().Set to set the content type to application/json
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	// Now, we write the response to the response body
+	_, err = w.Write(response)
+	if err != nil {
+		log.Panic(err)
+	}
+}
